246b16d9adf5ade9415c1fa8b789dc0115524f08
pimaxx69 <pepemaxx06@gmail.com>
2026-04-23 09:12:14 +0430

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,87 +1,52 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  firstName: {
-    type: String,
-    trim: true,
-    default: ''
-  },
-  lastName: {
-    type: String,
-    trim: true,
-    default: ''
-  },
-  coins: {
-    type: Number,
-    default: 0
-  },
-  pi: {
-    type: Number,
-    default: 0
-  },
-  usdt: {
-    type: Number,
-    default: 0
-  },
-  miningStart: {
-    type: Date,
-    default: null
-  },
-  lastMiningClaim: {
-    type: Date,
-    default: null
-  },
-  lastManualMining: {
-    type: Date,
-    default: null
-  },
+  userId: { type: String, required: true, unique: true },
+  email: { type: String, sparse: true },
+  firstName: { type: String, default: '' },
+  lastName: { type: String, default: '' },
+  coins: { type: Number, default: 0 },
+  pi: { type: Number, default: 0 },
+  usdt: { type: Number, default: 0 },
+  miningStart: { type: Number, default: null },
+  friendsInvited: { type: Number, default: 0 },
+  friendsActive: { type: Number, default: 0 },
+  invitedFriends: [{
+    id: String,
+    username: String,
+    inviteDate: Date,
+    isActive: Boolean
+  }],
+  activeFriends: [{
+    id: String,
+    username: String,
+    activeDate: Date
+  }],
+  rewardedFriends: [String],
+  completedTasks: [String],
+  taskStatuses: { type: Map, of: String, default: {} },
+  lastResetDate: { type: String, default: () => new Date().toDateString() },
   referrer: {
-    type: String,
-    default: null
+    id: String,
+    username: String,
+    date: Date
   },
-  referralLevel: {
-    type: Number,
-    default: 1
+  pendingRewards: [{
+    id: String,
+    username: String,
+    amount: Number,
+    type: String
+  }],
+  referralLevel: { type: Number, default: 1 },
+  totalReferralRewards: { type: Number, default: 0 },
+  activeInvestment: {
+    planId: String,
+    totalBase: Number,
+    startTimestamp: Number,
+    durationSeconds: Number,
+    profitRatePerDay: Number
   },
-  totalReferralRewards: {
-    type: Number,
-    default: 0
-  },
-  completedTasks: {
-    type: [String],
-    default: []
-  },
-  lastResetDate: {
-    type: Date,
-    default: Date.now
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now
-  }
+  lastManual: { type: Number, default: 0 }
 }, { timestamps: true });
-
-// Virtual for getting full name
-UserSchema.virtual('fullName').get(function() {
-  if (this.firstName && this.lastName) {
-    return `${this.firstName} ${this.lastName}`;
-  } else if (this.firstName) {
-    return this.firstName;
-  } else if (this.lastName) {
-    return this.lastName;
-  } else {
-    return 'User';
-  }
-});
 
 module.exports = mongoose.model('User', UserSchema);
